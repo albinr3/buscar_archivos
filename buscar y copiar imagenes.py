619cdb812c33d37391ffb2e429f25ce82647fad1
Albin Rodriguez <albinmrodriguez@gmail.com>
2022-03-16 17:52:04 -0400

    subiendo archivo modificado

--- buscar y copiar imagenes.py
+++ buscar y copiar imagenes.py
@@ -2,6 +2,7 @@ import os, shutil, xlrd
 
 #creamos una lista vacia para almacenar los codigos posteriormente
 files_to_find = []
+lista_imagenes_total=[]
 ruta_destino = 'C:/Users/Albin Rodriguez/Pictures/carpeta4' #'input("Digite la ruta de la carpeta destino: ")
 
 #Abrimos el archivo excel y cargamos todos los codigos en la lista vacia.
@@ -16,7 +17,14 @@ for root, dirs, files in os.walk('C:/Users/Albin Rodriguez/Desktop/FOTOS PRODUCT
         if _file in files_to_find:
             print (f'{_file} Encontrado en esta ruta: ' + str(root)) # Si lo encontramos nos dira que fue encontrado en esta ruta
             shutil.copy(os.path.abspath(root + '/' + _file), 'C:/Users/Albin Rodriguez/Pictures/carpeta4')
-      
+        lista_imagenes_total.append(_file)
+
+#Verificamos cuales archivos no fueron encontrados y lo imprimimos
+for elementos in files_to_find:
+    if elementos not in lista_imagenes_total:
+        print(f"El archivo {elementos} no fue encontrado!")        
         
+ 
+   
             
            
